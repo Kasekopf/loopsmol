940e01f412eba842a0c2346e2d5d54e2f6f54a41
Kasekopf <jeffreydudek@gmail.com>
2023-10-17 22:18:00 -0700

    Add diet

---
+++ src/tasks/diet.ts
@@ -0,0 +1,82 @@
+import {
+  cliExecute,
+  drink,
+  eat,
+  myAdventures,
+  myFullness,
+  myInebriety,
+  restoreMp,
+  reverseNumberology,
+  use,
+  useSkill,
+} from "kolmafia";
+import { $effects, $item, $items, $skill, get, have } from "libram";
+import { Priorities } from "../engine/priority";
+import { Quest } from "../engine/task";
+import { atLevel } from "../lib";
+
+export const DietQuest: Quest = {
+  name: "Diet",
+  tasks: [
+    {
+      name: "Eat",
+      ready: () =>
+        atLevel(5) &&
+        have($item`Ol' Scratch's salad fork`) &&
+        ((!get("calzoneOfLegendEaten") && have($item`Calzone of Legend`)) ||
+          (!get("deepDishOfLegendEaten") && have($item`Pizza of Legend`)) ||
+          (!get("pizzaOfLegendEaten") && have($item`Deep Dish of Legend`))),
+      completed: () => myFullness() > 0,
+      do: () => {
+        restoreMp(20);
+        useSkill($skill`Cannelloni Cocoon`);
+        eat(1, $item`Ol' Scratch's salad fork`);
+        if (!get("calzoneOfLegendEaten") && have($item`Calzone of Legend`)) {
+          eat(1, $item`Calzone of Legend`);
+        } else if (!get("deepDishOfLegendEaten") && have($item`Pizza of Legend`)) {
+          eat(1, $item`Pizza of Legend`);
+        } else if (!get("pizzaOfLegendEaten") && have($item`Deep Dish of Legend`)) {
+          eat(1, $item`Deep Dish of Legend`);
+        }
+      },
+      outfit: {
+        equip: $items`nurse's hat, sea salt scrubs`,
+        modifier: "100 hot res, HP",
+      },
+      limit: { tries: 1 },
+    },
+    {
+      name: "Drink",
+      ready: () => atLevel(11) && have($item`Frosty's frosty mug`),
+      completed: () =>
+        myInebriety() === 1 || (!have($item`astral pilsner`) && !have($item`astral six-pack`)),
+      do: () => {
+        if (have($item`astral six-pack`)) use($item`astral six-pack`);
+        restoreMp(20);
+        useSkill($skill`Cannelloni Cocoon`);
+        drink(1, $item`Frosty's frosty mug`);
+        drink(1, $item`astral pilsner`);
+      },
+      outfit: {
+        equip: $items`nurse's hat, sea salt scrubs`,
+        modifier: "100 cold res, HP",
+      },
+      effects: $effects`Ode to Booze`,
+      limit: { tries: 1 },
+    },
+    {
+      name: "Numberology",
+      priority: () => Priorities.Free,
+      after: ["Summon/War Frat 151st Infantryman"],
+      completed: () =>
+        get("_universeCalculated") >= get("skillLevel144") || get("_universeCalculated") >= 3,
+      ready: () => myAdventures() > 0 && Object.keys(reverseNumberology()).includes("69"),
+      do: (): void => {
+        restoreMp(1);
+        cliExecute("numberology 69");
+      },
+      limit: { tries: 5 },
+      freeaction: true,
+    },
+  ],
+};
