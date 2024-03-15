ab3e7a940cd2d8fb6d1f15176d745bc402914987
Build Script <>
2024-03-15 07:28:21 +0000

    Build Mafia files for commit 2a42f37

--- scripts/loopsmol/loopsmol.js
+++ scripts/loopsmol/loopsmol.js
@@ -12679,7 +12679,7 @@ function dailyDungeonTask() {
     combat: new combat_CombatStrategy().macro(() => {
       if (!property_get("_dailyDungeonMalwareUsed") && lib_have(template_string_$item(keys_templateObject33 || (keys_templateObject33 = keys_taggedTemplateLiteral(["daily dungeon malware"]))))) return Macro.item(template_string_$item(keys_templateObject34 || (keys_templateObject34 = keys_taggedTemplateLiteral(["daily dungeon malware"]))));
       return new Macro();
-    }).kill(),
+    }).killHard(),
     choices: () => {
       return {
         689: 1,
@@ -20108,7 +20108,7 @@ function checkRequirements() {
   }
 }
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "e5d9f0a";
+var lastCommitHash = "2a42f37";
 ;// CONCATENATED MODULE: ./src/main.ts
 var main_templateObject, main_templateObject2;
 function main_createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = main_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
